c970cc36fa11309f97b9a27f2b09985a53fe55c3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-10 20:05:35 +0000

    Add locations service

---
+++ src/SFA.DAS.EmployerDemand.Application.UnitTests/Locations/Services/WhenGettingLocations.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Application.Locations.Services;
+using SFA.DAS.EmployerDemand.Domain.Interfaces;
+using SFA.DAS.EmployerDemand.Domain.Locations.Api;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerDemand.Application.UnitTests.Locations.Services
+{
+    public class WhenGettingLocations
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Data_Is_Returned_From_The_Api(
+            string searchTerm,
+            string baseUrl,
+            Domain.Locations.Locations apiResponse,
+            [Frozen] Mock<IApiClient> apiClient,
+            LocationService service)
+        {
+            //Arrange
+            apiClient.Setup(x =>
+                    x.Get<Domain.Locations.Locations>(
+                        It.Is<GetLocationsApiRequest>(c => c.GetUrl.Contains(searchTerm))))
+                .ReturnsAsync(apiResponse);
+            
+            //Act
+            var actual = await service.GetLocations(searchTerm);
+            
+            //Assert
+            actual.Should().BeEquivalentTo(apiResponse);
+        }
+    }
+}
