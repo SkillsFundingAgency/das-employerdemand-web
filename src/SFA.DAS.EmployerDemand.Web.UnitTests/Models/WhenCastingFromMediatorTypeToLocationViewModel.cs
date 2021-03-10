2c49f784dd5bb9cb86429b102dea05884a30beff
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-10 20:23:25 +0000

    Add location item view model

---
+++ src/SFA.DAS.EmployerDemand.Web.UnitTests/Models/WhenCastingFromMediatorTypeToLocationViewModel.cs
@@ -0,0 +1,20 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Web.Models;
+
+namespace SFA.DAS.EmployerDemand.Web.UnitTests.Models
+{
+    public class WhenCastingFromMediatorTypeToLocationViewModel
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Correctly_Mapped(Domain.Locations.Locations.LocationItem source)
+        {
+            //Act
+            var actual = (LocationViewModel) source;
+
+            //Assert
+            source.Should().BeEquivalentTo(actual);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand.Web/Models/LocationViewModel.cs
@@ -0,0 +1,15 @@
+namespace SFA.DAS.EmployerDemand.Web.Models
+{
+    public class LocationViewModel
+    {
+        public string Name { get; set; }
+        
+        public static implicit operator LocationViewModel(Domain.Locations.Locations.LocationItem source)
+        {
+            return new LocationViewModel
+            {
+                Name = source.Name
+            };
+        }
+    }
+}
