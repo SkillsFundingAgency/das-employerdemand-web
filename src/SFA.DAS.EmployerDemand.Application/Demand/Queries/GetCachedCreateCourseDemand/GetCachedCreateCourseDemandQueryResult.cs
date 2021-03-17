108ab6a23259eb9a2e0fd03965987071cc22d18f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-17 16:49:18 +0000

    Add handler for getting cached CreateCourseDemand request

---
+++ src/SFA.DAS.EmployerDemand.Application/Demand/Queries/GetCachedCreateCourseDemand/GetCachedCreateCourseDemandQueryResult.cs
@@ -0,0 +1,10 @@
+using SFA.DAS.EmployerDemand.Domain.Demand;
+using SFA.DAS.EmployerDemand.Domain.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.Application.Demand.Queries.GetCachedCreateCourseDemand
+{
+    public class GetCachedCreateCourseDemandQueryResult
+    {
+        public CourseDemandRequest CourseDemand { get ; set ; }
+    }
+}
