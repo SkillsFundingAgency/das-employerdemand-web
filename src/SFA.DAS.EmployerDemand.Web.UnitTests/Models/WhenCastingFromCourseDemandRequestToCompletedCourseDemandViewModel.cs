adf1d3ca8c2a029a49508f5642f780a277d848d6
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-18 16:37:17 +0000

    Add CompletedCourseDemandViewModel

---
+++ src/SFA.DAS.EmployerDemand.Web.UnitTests/Models/WhenCastingFromCourseDemandRequestToCompletedCourseDemandViewModel.cs
@@ -0,0 +1,32 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Domain.Demand;
+using SFA.DAS.EmployerDemand.Web.Models;
+
+namespace SFA.DAS.EmployerDemand.Web.UnitTests.Models
+{
+    public class WhenCastingFromCourseDemandRequestToCompletedCourseDemandViewModel
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Correctly_Mapped(CourseDemandRequest source)
+        {
+            //Act
+            var actual = (CompletedCourseDemandViewModel) source;
+            
+            //Assert
+            actual.TrainingCourse.Should().BeEquivalentTo(source.Course);
+            actual.LocationName.Should().Be(source.LocationItem.Name);
+        }
+
+        [Test]
+        public void Then_If_The_Source_Is_Null_Then_Null_Returned()
+        {
+            //Act
+            var actual = (CompletedCourseDemandViewModel) (CourseDemandRequest) null;
+            
+            //Assert
+            actual.Should().BeNull();           
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand.Web/Models/CompletedCourseDemandViewModel.cs
@@ -0,0 +1,27 @@
+using System;
+using SFA.DAS.EmployerDemand.Domain.Demand;
+
+namespace SFA.DAS.EmployerDemand.Web.Models
+{
+    public class CompletedCourseDemandViewModel
+    {
+        public Guid Id { get; set; }
+        public TrainingCourseViewModel TrainingCourse { get; set; }
+        public string LocationName { get ; set ; }
+
+        public static implicit operator CompletedCourseDemandViewModel (CourseDemandRequest source)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+            
+            return new CompletedCourseDemandViewModel
+            {
+                Id = source.Id,
+                TrainingCourse = source.Course,
+                LocationName = source.LocationItem.Name,
+            };
+        }
+    }
+}
