04f72b313bb78291e8d05c72457c03e210e41f18
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-26 08:34:04 +0000

    Add ProviderIdams configuration

---
+++ src/SFA.DAS.EmployerDemand.Domain/Configuration/ProviderIdams.cs
@@ -0,0 +1,9 @@
+namespace SFA.DAS.EmployerDemand.Domain.Configuration
+{
+    public class ProviderIdams
+    {
+        public string MetadataAddress { get; set; }
+
+        public string Wtrealm  {get; set; }
+    }
+}
