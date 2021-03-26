38c2efaba4798487cf2b53fbfa6d113009470ea2
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-26 08:21:19 +0000

    Add provider authorisation handler

---
+++ src/SFA.DAS.EmployerDemand.Web.UnitTests/Infrastructure/Authorization/WhenHandlingProviderAuthorizationRequirement.cs
@@ -0,0 +1,91 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Web.Infrastructure.Authorization;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerDemand.Web.UnitTests.Infrastructure.Authorization
+{
+    public class WhenHandlingProviderAuthorizationRequirement
+    {
+        
+
+        [Test, MoqAutoData]
+        public async Task Then_Fails_If_No_Provider_Ukprn_Claim(
+            int ukprn,
+            ProviderUkPrnRequirement orIsProviderRequirement,
+            Endpoint endpointResource,
+            [Frozen]Mock<IHttpContextAccessor> httpContextAccessor,
+            ProviderAuthorizationHandler provider)
+        {
+            //Arrange
+            var claim = new Claim("NotProviderClaim", ukprn.ToString());
+            var claimsPrinciple = new ClaimsPrincipal(new[] {new ClaimsIdentity(new[] {claim})});
+            var context = new AuthorizationHandlerContext(new []{orIsProviderRequirement},claimsPrinciple, endpointResource);
+            
+            //Act
+            await provider.HandleAsync(context);
+
+            //Assert
+            Assert.IsFalse(context.HasSucceeded);
+            Assert.IsTrue(context.HasFailed);
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_If_The_Route_Ukprn_Does_Not_Match_Value_In_Claim_Then_Fails(
+            int ukprn,
+            int routeUkprn,
+            ProviderUkPrnRequirement orIsProviderRequirement,
+            Endpoint endpointResource,
+            [Frozen]Mock<IHttpContextAccessor> httpContextAccessor,
+            ProviderAuthorizationHandler provider)
+        {
+            
+            var responseMock = new FeatureCollection();
+            var httpContext = new DefaultHttpContext(responseMock);
+            httpContext.Request.RouteValues.Add("ukprn",routeUkprn);
+            httpContextAccessor.Setup(_ => _.HttpContext).Returns(httpContext);
+            var claim = new Claim(ProviderClaims.ProviderUkprn, ukprn.ToString());
+            var claimsPrinciple = new ClaimsPrincipal(new[] {new ClaimsIdentity(new[] {claim})});
+            var context = new AuthorizationHandlerContext(new []{orIsProviderRequirement},claimsPrinciple, endpointResource);
+            
+            //Act
+            await provider.HandleAsync(context);
+
+            //Assert
+            Assert.IsFalse(context.HasSucceeded);
+            Assert.IsTrue(context.HasFailed);
+        }
+        
+        
+        [Test, MoqAutoData]
+        public async Task Then_If_The_Route_Ukprn_Does_Match_Value_In_Claim_Then_Succeeds(
+            int ukprn,
+            ProviderUkPrnRequirement orIsProviderRequirement,
+            Endpoint endpointResource,
+            [Frozen]Mock<IHttpContextAccessor> httpContextAccessor,
+            ProviderAuthorizationHandler provider)
+        {
+            
+            var responseMock = new FeatureCollection();
+            var httpContext = new DefaultHttpContext(responseMock);
+            httpContext.Request.RouteValues.Add("ukprn",ukprn);
+            httpContextAccessor.Setup(_ => _.HttpContext).Returns(httpContext);
+            var claim = new Claim(ProviderClaims.ProviderUkprn, ukprn.ToString());
+            var claimsPrinciple = new ClaimsPrincipal(new[] {new ClaimsIdentity(new[] {claim})});
+            var context = new AuthorizationHandlerContext(new []{orIsProviderRequirement},claimsPrinciple, endpointResource);
+            
+            //Act
+            await provider.HandleAsync(context);
+
+            //Assert
+            Assert.IsTrue(context.HasSucceeded);
+            Assert.IsFalse(context.HasFailed);
+        }
+    }
+}
