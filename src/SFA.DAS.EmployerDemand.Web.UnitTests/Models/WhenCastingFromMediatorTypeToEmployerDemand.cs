ab984619ff4acec61348b0892dc8432444c7464c
Peter Williams <peter.david.williams.1991@gmail.com>
2021-05-05 12:27:48 +0100

    Added test for EmploerDemand cast

---
+++ src/SFA.DAS.EmployerDemand.Web.UnitTests/Models/WhenCastingFromMediatorTypeToEmployerDemand.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerDemand.Web.UnitTests.Models
+{
+    public class WhenCastingFromMediatorTypeToEmployerDemand
+    {
+        [Test, MoqAutoData]
+        public void Then_The_Fields_Are_Mapped_Correctly(Domain.Demand.EmployerDemands source)
+        {
+            var actual = (Web.Models.EmployerDemands) source;
+
+            actual.Should().BeEquivalentTo(source);
+        }
+    }
+}
