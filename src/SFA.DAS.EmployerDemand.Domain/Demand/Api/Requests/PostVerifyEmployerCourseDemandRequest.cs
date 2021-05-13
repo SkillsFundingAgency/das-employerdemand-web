a45eaa99ce16321492f6e3d55c50addf7b53a2b2
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-05-13 16:09:39 +0100

    Add post request to verify demand

---
+++ src/SFA.DAS.EmployerDemand.Domain/Demand/Api/Requests/PostVerifyEmployerCourseDemandRequest.cs
@@ -0,0 +1,17 @@
+using System;
+using SFA.DAS.EmployerDemand.Domain.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.Domain.Demand.Api.Requests
+{
+    public class PostVerifyEmployerCourseDemandRequest : IPostApiRequest
+    {
+        private readonly Guid _id;
+
+        public PostVerifyEmployerCourseDemandRequest(Guid id)
+        {
+            _id = id;
+        }
+
+        public string PostUrl => $"demand/{_id}/verify";
+    }
+}
