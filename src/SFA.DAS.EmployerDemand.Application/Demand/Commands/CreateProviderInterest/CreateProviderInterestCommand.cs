f89cb688195f1f0e2c39c554ee8f1668ebabde34
Scott Wakefield <scott@expertcc.co.uk>
2021-05-14 17:18:25 +0100

    AEDB-34 added handler for create.

---
+++ src/SFA.DAS.EmployerDemand.Application/Demand/Commands/CreateProviderInterest/CreateProviderInterestCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MediatR;
+
+namespace SFA.DAS.EmployerDemand.Application.Demand.Commands.CreateProviderInterest
+{
+    public class CreateProviderInterestCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+    }
+}
