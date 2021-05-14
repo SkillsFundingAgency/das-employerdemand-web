231f011c1024494494b0137448d7c8f9a4e0fc21
Scott Wakefield <scott@expertcc.co.uk>
2021-05-14 16:18:20 +0100

    AEDB-34 created api request and payload.

---
+++ src/SFA.DAS.EmployerDemand.Domain.UnitTests/Demand/Api/WhenBuildingThePostCreateProviderInterestsData.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Domain.Demand.Api.Requests;
+using SFA.DAS.EmployerDemand.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerDemand.Domain.UnitTests.Demand.Api
+{
+    public class WhenBuildingThePostCreateProviderInterestsData
+    {
+        [Test, MoqAutoData]
+        public void Then_Maps_Fields(IProviderDemandInterest interest)
+        {
+            var data = new PostCreateProviderInterestsData(interest);
+
+            data.EmployerDemandIds.Should()
+                .BeEquivalentTo(interest.EmployerDemands
+                    .Select(demands => demands.EmployerDemandId));
+            data.Ukprn.Should().Be(interest.Ukprn);
+            data.Email.Should().Be(interest.EmailAddress);
+            data.Phone.Should().Be(interest.PhoneNumber);
+            data.Website.Should().Be(interest.Website);
+        }
+    }
+}
