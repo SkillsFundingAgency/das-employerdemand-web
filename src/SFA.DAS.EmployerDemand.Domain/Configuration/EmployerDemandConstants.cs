5b663e490df43f5bee3b38271bcc0118825bed68
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-05-14 10:02:53 +0100

    Add protector constant name value

---
+++ src/SFA.DAS.EmployerDemand.Domain/Configuration/EmployerDemandConstants.cs
@@ -0,0 +1,7 @@
+namespace SFA.DAS.EmployerDemand.Domain.Configuration
+{
+    public static class EmployerDemandConstants
+    {
+        public static string EmployerDemandProtectorName = "EmployerDemand";
+    }
+}
