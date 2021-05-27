0128b38e4cc5b5ad87e516890b1c8073d0f746bc
Peter Williams <peter.david.williams.1991@gmail.com>
2021-05-27 16:38:44 +0100

    Added redirect to FAT courses page if the course LastStartDate has passed, added test for redirect

--- src/SFA.DAS.EmployerDemand.Web.UnitTests/Controllers/Demand/WhenConfirmingRegisterDemand.cs
+++ src/SFA.DAS.EmployerDemand.Web.UnitTests/Controllers/Demand/WhenConfirmingRegisterDemand.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Security.Policy;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmployerDemand.Application.Demand.Queries.GetCachedCreateCourseDemand;
@@ -26,6 +28,7 @@ namespace SFA.DAS.EmployerDemand.Web.UnitTests.Controllers.Demand
             [Greedy] RegisterDemandController controller)
         {
             //Arrange
+            mediatorResult.CourseDemand.Course.LastStartDate = DateTime.Now.AddDays(1);
             mediator.Setup(x =>
                     x.Send(It.Is<GetCachedCreateCourseDemandQuery>(c => 
                             c.Id.Equals(demandId))
@@ -67,6 +70,36 @@ namespace SFA.DAS.EmployerDemand.Web.UnitTests.Controllers.Demand
             actual.RouteName.Should().Be(RouteNames.RegisterDemand);
             actual.RouteValues.ContainsKey("Id").Should().BeTrue();
             actual.RouteValues["Id"].Should().Be(courseId);
+        }
+
+        [Test, MoqAutoData]
+        public async Task
+            Then_If_The_Course_LastStartDate_Is_In_The_Past_Then_Redirected_To_FindApprenticeshipTraining_Course_List(
+                int courseId,
+                Guid demandId,
+                string baseUrl,
+                GetCachedCreateCourseDemandQueryResult mediatorResult,
+                [Frozen] Mock<IOptions<Domain.Configuration.EmployerDemand>> config,
+                [Frozen] Mock<IMediator> mediator,
+                [Greedy] RegisterDemandController controller)
+        {
+            //Arrange
+            config.Object.Value.FindApprenticeshipTrainingUrl = baseUrl;
+            mediatorResult.CourseDemand.Course.LastStartDate = DateTime.Today.AddDays(-1);
+            mediator.Setup(x =>
+                    x.Send(It.Is<GetCachedCreateCourseDemandQuery>(c =>
+                            c.Id.Equals(demandId))
+                        , It.IsAny<CancellationToken>()))
+                .ReturnsAsync(mediatorResult);
+
+            //Act
+            var actual = await controller.ConfirmRegisterDemand(courseId, demandId) as RedirectResult;
+
+            //Assert
+            Assert.IsNotNull(actual);
+            actual.Url.Should().Be($"{baseUrl}/courses/{mediatorResult.CourseDemand.Course.Id}");
+            actual.Permanent.Should().BeFalse();
+            actual.PreserveMethod.Should().BeTrue();
         }
     }
 }
--- src/SFA.DAS.EmployerDemand.Web/Controllers/RegisterDemandController.cs
+++ src/SFA.DAS.EmployerDemand.Web/Controllers/RegisterDemandController.cs
@@ -105,7 +105,12 @@ namespace SFA.DAS.EmployerDemand.Web.Controllers
             {
                 return RedirectToRoute(RouteNames.RegisterDemand, new {Id = id});
             }
-           
+
+            if (model.TrainingCourse.LastStartDate != null && model.TrainingCourse.LastStartDate < DateTime.Now)
+            {
+                return new RedirectResult($"{_config.FindApprenticeshipTrainingUrl}/courses/{model.TrainingCourse.Id}", false, true);
+            }
+
             return View(model);
         }
 
@@ -154,7 +159,6 @@ namespace SFA.DAS.EmployerDemand.Web.Controllers
             {
                 return RedirectToRoute(RouteNames.RegisterDemand, new {Id = id});
             }
-
 
             model.FindApprenticeshipTrainingCourseUrl = _config.FindApprenticeshipTrainingUrl + "/courses";
            
