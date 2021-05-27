39349ba8bd5fdb850dd824475805f3b8a9d21a6f
A-J-Wright <alissa.wright@education.gov.uk>
2021-05-27 16:57:04 +0100

    Update content based on AEDB58 Copydeck

--- src/SFA.DAS.EmployerDemand.Web.UnitTests/Models/WhenCastingFromProviderInterestRequestToReviewProviderDetailsViewModel.cs
+++ src/SFA.DAS.EmployerDemand.Web.UnitTests/Models/WhenCastingFromProviderInterestRequestToReviewProviderDetailsViewModel.cs
@@ -14,7 +14,6 @@ namespace SFA.DAS.EmployerDemand.Web.UnitTests.Models
             var actual = (ReviewProviderDetailsViewModel) source;
 
             actual.Should().BeEquivalentTo(source, options => options
-                .Excluding(request => request.ProviderOffersThisCourse)
             );
             actual.RouteDictionary["ukprn"].Should().Be(source.Ukprn.ToString());
             actual.RouteDictionary["courseId"].Should().Be(source.Course.Id.ToString());
