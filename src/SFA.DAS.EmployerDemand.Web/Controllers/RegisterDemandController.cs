688b8b4a0ba6b147f9529b3d9f3442b9e92c110d
PF2RZXTY\Pete <peter.david.williams.1991@gmail.com>
2021-06-02 09:45:57 +0100

    Changed datetime comparison for PR comment

--- src/SFA.DAS.EmployerDemand.Web/Controllers/RegisterDemandController.cs
+++ src/SFA.DAS.EmployerDemand.Web/Controllers/RegisterDemandController.cs
@@ -106,7 +106,7 @@ namespace SFA.DAS.EmployerDemand.Web.Controllers
                 return RedirectToRoute(RouteNames.RegisterDemand, new {Id = id});
             }
 
-            if (model.TrainingCourse.LastStartDate != null && model.TrainingCourse.LastStartDate < DateTime.Now)
+            if (model.TrainingCourse.LastStartDate != null && model.TrainingCourse.LastStartDate < DateTime.Today)
             {
                 return new RedirectResult($"{_config.FindApprenticeshipTrainingUrl}courses/{model.TrainingCourse.Id}", false, true);
             }
