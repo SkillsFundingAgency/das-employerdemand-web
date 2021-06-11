f997537b33a9960d9f5239620f430a7fe4712896
Scott Wakefield <scott@expertcc.co.uk>
2021-06-11 16:03:16 +0100

    AEDB-61 add config and partial for provider zendesk widget.

--- src/SFA.DAS.EmployerDemand.Web/AppStart/AddConfigurationOptionsExtension.cs
+++ src/SFA.DAS.EmployerDemand.Web/AppStart/AddConfigurationOptionsExtension.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using SFA.DAS.EmployerDemand.Domain.Configuration;
+using SFA.DAS.Provider.Shared.UI.Models;
 
 namespace SFA.DAS.EmployerDemand.Web.AppStart
 {
@@ -16,6 +17,8 @@ namespace SFA.DAS.EmployerDemand.Web.AppStart
             services.AddSingleton(cfg => cfg.GetService<IOptions<Domain.Configuration.EmployerDemand>>().Value);
             services.Configure<ProviderIdams>(configuration.GetSection("ProviderIdams"));
             services.AddSingleton(cfg => cfg.GetService<IOptions<ProviderIdams>>().Value);
+            services.Configure<ZenDeskConfiguration>(configuration.GetSection("ZenDeskSettings"));
+            services.AddSingleton(cfg => cfg.GetService<IOptions<ZenDeskConfiguration>>().Value);
         }
     }
 }
