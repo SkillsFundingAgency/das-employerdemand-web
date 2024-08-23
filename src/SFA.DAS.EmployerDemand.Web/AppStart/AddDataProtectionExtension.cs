e981e5d9bbcda026d82fbed2e7a00791df7c501a
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-08-23 22:46:54 +0100

    FAI-1063 - Update data protector application name

--- src/SFA.DAS.EmployerDemand.Web/AppStart/AddDataProtectionExtension.cs
+++ src/SFA.DAS.EmployerDemand.Web/AppStart/AddDataProtectionExtension.cs
@@ -24,7 +24,7 @@ namespace SFA.DAS.EmployerDemand.Web.AppStart
                     .Connect($"{redisConnectionString},{dataProtectionKeysDatabase}");
 
                 services.AddDataProtection()
-                    .SetApplicationName("das-provider")
+                    .SetApplicationName("das-provider-web")
                     .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
             }
         }
